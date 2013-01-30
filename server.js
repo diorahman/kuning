3da9db9c39e3bcb78321438f446d2c41af7f1225
Dhi Aurrahman <diorahman@gmail.com>
2013-01-30 15:40:56 +0700

    change port for heroku deployment

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express')
 var server = express()
 server.use(express.static(__dirname + '/public'))
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
+server.listen(port, function() {
   console.log("Listening on " + port);
 });
